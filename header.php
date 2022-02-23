62ae1b486f4c24acab3bd801c1440138793fbe89
DeepikaAnbalagan <deepideep61@gmail.com>
2022-02-23 14:08:06 +0530

    commit

--- header.php
+++ header.php
@@ -5,34 +5,111 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500&family=Dancing+Script&family=Dosis:wght@200&family=IBM+Plex+Serif:wght@100&family=Josefin+Sans:ital@1&family=Lato&family=Libre+Baskerville&family=Open+Sans+Condensed:wght@300&family=Quicksand:wght@300&display=swap" rel="stylesheet">
+
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-sm bg-light1">
+  <div class="container">
+    <div class="nav-top">
+  <ul class="navbar-nav">
+    <li class="nav-items active">
+     <span> <a class="nav-link1" href="#">Blog</a>  </span>
+    </li>
+    <li class="nav-items">
+    <span>  <a class="nav-link1" href="#">Portfolio</a>  </span>
+    </li>
+    <li class="nav-items">
+    <span>  <a class="nav-link1" href="#">Contact</a>  </span>
+    </li>
 
-<nav class="top">
-  <div class="container wrapper">
-    <!-- <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
-    </div> -->
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Home</a></li>
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Page 1-1g</a></li>
-          <li><a href="#">Page 1-2</a></li>
-          <li><a href="#">Page 1-3</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Page 2</a></li>
-    </ul>
-    <ul class="nav navbar-nav navbar-right">
-      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-    </ul>
+    <li class="nav-items">
+    <span> <a class="nav-link1 " href="#">PageTemplate</a>  </span>
+    </li>
+    <li class="nav-items">
+    <span><a class="nav-link1 " href="#">Shop</a>  </span>
+    </li>
+  </ul>
   </div>
+<div class="navs">
+<ul class="navbar-nav">
+    <li class="nav-item active">
+     <span> <a class="nav-link" href="#"><i class="fa fa-facebook-f"  style="font-size:16px;color: black;"></i></a>  </span>
+    </li>
+    <li class="nav-item">
+    <span>  <a class="nav-link" href="#"><i class="fa fa-twitter" style="font-size:16px;color: black;"></i></a>  </span>
+    </li>
+    <li class="nav-item">
+    <span>  <a class="nav-link" href="#"><i class="fa fa-linkedin-square" style="font-size:16px;color: black;"></i></a>  </span>
+    </li>
+    <li class="nav-item">
+    <span>  <a class="nav-link" href="#"><i class="fa fa-instagram" style="font-size:16px;color: black;"></i></a>  </span>
+    </li>
+</div>
+
+  
 </nav>
-</body>
-</html>
+        <!-- End top header-->
+
+
+        <div class="navigation-wrap bg-light start-header start-style sticky-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<nav class="navbar navbar-expand-md navbar-light">
+					
+						<a class="navbar-brand" href="https://front.codes/" target="_blank">
+            <img class="logo-img" itemprop="logo" src="https://www.demos.machothemes.com/antreas/wp-content/uploads/sites/22/2018/11/antreas-logo.png" width="90" height="43" alt="Antreas">
+            </a>	
+						
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav ml-auto py-4 py-md-0">
+								<li class="nav-item">
+									<a class="nav-link " href="#">Features</a>
+							
+								</li>
+                	<li class="nav-item">
+									<a class="nav-link" href="#">About</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Portfolio</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link" href="#">Service</a>
+								</li>
+		
+								<li class="nav-item">
+									<a class="nav-link" href="#">Testimonial</a>
+								</li>
+								<li class="nav-item ">
+									<a class="nav-link" href="#">Clients</a>
+								</li>
+                <li class="nav-item">
+									<a class="nav-link" href="#">Team</a>
+								</li>
+                <li class="nav-item">
+									<a class="nav-link" href="#">Contact</a>
+								</li>
+							</ul>
+						</div>
+						
+					</nav>		
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+  
