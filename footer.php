15400fe1ba34ae1fbee723b8fe46cd9aecb00913
Keerthi-U <keerthiiachu173@gmail.com>
2022-02-23 14:11:38 +0530

    added pages

---
+++ footer.php
@@ -0,0 +1,128 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css"> -->
+
+<!-- jQuery library -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script> -->
+
+<!-- Popper JS -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script> -->
+
+<!-- Latest compiled JavaScript -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script> -->
+<!-- GOOGLE FONT -->
+<!-- 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<link href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500&family=Dancing+Script&family=Dosis:wght@200&family=IBM+Plex+Serif:wght@100&family=Josefin+Sans:ital@1&family=Lato&family=Libre+Baskerville&family=Open+Sans+Condensed:wght@300&family=Quicksand:wght@300&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+ <!-- social media cdn  -->
+<!-- <link rel="stylesheet" href="maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style.css">
+    <title>Document</title>
+
+    <style>
+
+
+
+   </style>
+</head>
+<body> -->
+
+ 
+
+<!-- footer-section start -->
+  <div id="subfooter" class="subfooter">
+      <div class="container">
+          <div class="row">
+            <div class="column col3">
+               <div class="subfooter-column">
+                   <div class="text-col-1" id="t-title">
+                        <div class="text-col">
+                         About us
+                        </div>
+                        <div class="textwidget"><p class="paragraph"> Etiam rutrum mattis metus vitae dapibus. <strong>Cras sagitti</strong>s leo tellus, non rhoncus velit <em>efficitur</em> tristique.</p>
+                            <h5 class="contact-info">Contact Info</h5>
+                            <p><i class="fa fa-map-marker"></i>&nbsp;&nbsp;<strong>Adress:</strong> 75 Ninth Avenue New York, NY 10011</p>
+                            <p><i class="fa fa-phone"></i>&nbsp;&nbsp;<strong>Telephone:</strong> +1 212-565-0000</p>
+                            <p><i class="fa fa-envelope"></i>&nbsp;&nbsp;<strong>Email:</strong> <a href="mailto:contact@gmail.com">contact@gmail.com</a></p>
+                        </div>
+
+                   </div>
+               </div>
+
+
+            </div>
+            <div class="column col3">
+               <div class="subfooter-column">
+                   <div class="text-col-1" id="t-title">
+                     <div class="text-col2">Antreas is social</div> 
+                     <div id="social">
+                        <a class="facebookBtn smGlobalBtn" href="#" ></a>
+                        <a class="twitterBtn smGlobalBtn" href="#" ></a>
+                        <a class="googleplusBtn smGlobalBtn" href="#" ></a>
+                        <a class="linkedinBtn smGlobalBtn" href="#" ></a>
+                        <a class="pinterestBtn smGlobalBtn" href="#" ></a>
+                        <a class="tumblrBtn smGlobalBtn" href="#" ></a>
+                        <a class="rssBtn smGlobalBtn" href="#" ></a>
+                     </div>
+                     <div class="text-col2">Search</div> 
+                     <div class="search-box">
+					<label class="screen-reader-text" for="s">Search for:</label>
+					<input type="text" value="" name="s" id="s">
+					<input type="submit" id="searchsubmit" value="Search">
+				     </div>
+                 </div>
+              </div>
+            </div>
+            <div class="column col3">
+            <div class="subfooter-column">
+                   <div class="text-col-1" id="t-title">
+                     <div class="text-col">Recent posts</div> 
+                        <div class="img-post-div">
+                            <div class="div1">
+                            <img src="image/img.jpg" class="air">
+                            <p class="texte">
+                            <a href="#" class="img-head">Travel Tips for South Africa</a><br>
+                            <span class="texts">   October 12, 2018</span>
+                            <p>
+                            </div>
+                            <div class="div1">
+                            <img src="image/img1.jpg" class="air">
+                            <p class="texte">
+                            <a href="#" class="img-head">Travel Tips for South Africa</a><br>
+                            <span class="texts">   October 12, 2018</span>
+                            <p>
+                           </div>
+                            <div class="div1">
+                            <img src="image/img2.jpg" class="air">
+                            <p class="texte" >
+                            <a href="#" class="img-head">Travel Tips for South Africa</a><br>
+                            <span class="texts">   October 12, 2018</span>
+                            <p>
+  
+                           </div>
+                        </div>
+                    
+                   </div>
+             </div>
+
+            </div>
+        </div>
+      </div>
+
+  </div>
+
+
+
+
+
+
+</body>
+</html>
